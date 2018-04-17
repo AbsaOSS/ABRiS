22391ac9d14c19924e239aae11471cc408cb06a6
melofeli <Felipe.Melo@barclayscapital.com>
2018-04-17 14:32:57 +0200

    Improving README.

--- spark-avro-dataframes/src/main/scala/za/co/absa/abris/examples/SampleAvroParquetExample.scala
+++ spark-avro-dataframes/src/main/scala/za/co/absa/abris/examples/SampleAvroParquetExample.scala
@@ -1,17 +1,29 @@
+/*
+ * Copyright 2018 Barclays Africa Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package za.co.absa.abris.examples
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.Dataset
-import za.co.absa.abris.examples.utils.ExamplesUtils
+import java.io.FileInputStream
+import java.util.Properties
+
+import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import za.co.absa.abris.avro.format.SparkAvroConversions
-import za.co.absa.abris.examples.data.generation.ComplexRecordsGenerator
 import za.co.absa.abris.avro.parsing.utils.AvroSchemaUtils
-import java.util.Properties
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.Encoder
-import java.io.FileInputStream
-import org.apache.spark.sql.SaveMode
+import za.co.absa.abris.examples.data.generation.ComplexRecordsGenerator
 
 object SampleAvroParquetExample {
 
@@ -36,9 +48,8 @@ object SampleAvroParquetExample {
 
   private def writeAvroToParquet(destination: String, numRecords: Int, spark: SparkSession) = {
     
-    import za.co.absa.abris.avro.AvroSerDe._
     import spark.implicits._
-    import ExamplesUtils._
+    import za.co.absa.abris.avro.AvroSerDe._
     
     implicit val encoder = getEncoder()
         
@@ -54,7 +65,6 @@ object SampleAvroParquetExample {
   
   private def readParquetAsAvro(source: String, spark: SparkSession): Dataset[Row] = {
     
-    import spark.implicits._
     import za.co.absa.abris.avro.AvroSerDe._
     
     spark
