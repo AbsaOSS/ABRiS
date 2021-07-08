7e8bc02325a93082c17a00a2b69be9a9781e4be1
yokhan-dukhin <johand84@gmail.com>
2021-07-08 12:10:55 -0400

    making FromAvroConfig and ToAvroConfig classes serializable

--- src/main/scala/za/co/absa/abris/config/Config.scala
+++ src/main/scala/za/co/absa/abris/config/Config.scala
@@ -144,7 +144,7 @@ class ToSchemaRegisteringConfigFragment(
   *
   * This builder allows us to add new properties in backward compatible manner.
   */
-class ToAvroConfig private(abrisConfig: Map[String, Any]) {
+class ToAvroConfig private(abrisConfig: Map[String, Any]) extends Serializable {
 
   import ToAvroConfig.Key
 
@@ -281,7 +281,7 @@ class FromConfluentAvroConfigFragment {
 class FromAvroConfig private(
   abrisConfig: Map[String, Any],
   schemaRegistryConf: Option[Map[String,String]]
-) {
+) extends Serializable {
 
   import FromAvroConfig.Key
 
