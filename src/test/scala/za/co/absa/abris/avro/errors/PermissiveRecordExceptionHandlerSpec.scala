00297b8fb1df015b1f82b3394855b6f16d1ad313
Willian Moreira <willian.moreira@ifood.com.br>
2023-03-27 22:25:38 -0300

    Adding PermissiveRecordExceptionHandle that retun all fields null if an error occur.

---
+++ src/test/scala/za/co/absa/abris/avro/errors/PermissiveRecordExceptionHandlerSpec.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.abris.avro.errors
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.avro.{AbrisAvroDeserializer, SchemaConverters}
+import org.apache.spark.sql.catalyst.expressions.SpecificInternalRow
+import org.apache.spark.sql.types.{DataType, StructField, StructType}
+import org.apache.spark.unsafe.types.UTF8String
+import org.scalatest.flatspec.AnyFlatSpec
+import za.co.absa.abris.avro.parsing.utils.AvroSchemaUtils
+import za.co.absa.abris.examples.data.generation.TestSchemas
+
+class PermissiveRecordExceptionHandlerSpec extends AnyFlatSpec {
+
+  private val spark = SparkSession
+    .builder()
+    .appName("unitTest")
+    .master("local[1]")
+    .config("spark.driver.bindAddress", "localhost")
+    .config("spark.ui.enabled", "false")
+    .getOrCreate()
+
+  it should "receive empty dataframe row back" in {
+
+    val expectedNestedFieldSchema = new StructType()
+      .add("int", "int")
+      .add("long", "long")
+    val expectedNestedStructSchema = new StructType()
+      .add("name", "string")
+      .add("nested", expectedNestedFieldSchema)
+
+    val expectedNestedFieldInternalRow = new SpecificInternalRow(expectedNestedFieldSchema)
+    expectedNestedFieldInternalRow.setNullAt(0)
+    expectedNestedFieldInternalRow.setNullAt(1)
+
+    val expectedNestedStructInternalRow = new SpecificInternalRow(expectedNestedStructSchema)
+    expectedNestedStructInternalRow.setNullAt(0)
+    expectedNestedStructInternalRow.setNullAt(1)
+
+    //actual
+    val deserializationExceptionHandler = new PermissiveRecordExceptionHandler()
+    val schema = AvroSchemaUtils.parse(TestSchemas.NATIVE_SIMPLE_OUTER_SCHEMA)
+    val dataType: DataType = SchemaConverters.toSqlType(schema).dataType
+
+    val actualResult = deserializationExceptionHandler
+      .handle(new Exception, new AbrisAvroDeserializer(schema, dataType), schema)
+
+    assert(actualResult == expectedNestedStructInternalRow)
+  }
+}
